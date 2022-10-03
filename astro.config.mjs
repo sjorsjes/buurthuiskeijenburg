d2f1d584b78b2b4c25549c7152748c9e5de8e927
Sjors Spoorendonk <sjors@vicompany.nl>
2022-10-03 14:03:42 +0200

    Remove unwanted packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [image()],
   site: 'https://sjorsjes.github.io',
 	base: '/buurthuiskeijenburg',
-  integrations: [mdx(), sitemap(), image()],
-  vite: {
-    assetsInclude: ['**/*.JPG']
-  }
 });
