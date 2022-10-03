eaccf95123a65f747415cd66335d038a45b332da
Sjors Spoorendonk <sjors@vicompany.nl>
2022-10-03 20:46:40 +0200

    Fix css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,19 +1,9 @@
 const atImport = require('postcss-import');
-const customProperties = require('postcss-custom-properties');
-const customMedia = require('postcss-custom-media');
 const nested = require('postcss-nested');
-const csso = require('postcss-csso');
 
 module.exports = {
 	plugins: [
 		atImport(),
-		customMedia(),
-		customProperties({
-			preserve: false,
-		}),
 		nested(),
-		csso({
-			sourceMap: true,
-		}),
 	],
 };
--- src/js/toggleMenu.js
+++
@@ -1,5 +0,0 @@
-const menu = document.querySelector('.js-toggle-menu');
-
-menu.addEventListener('click', () => {
-	document.body.classList.toggle('mobile-menu-active');
-});
