0966619ac27a827b0a500088c0e3a3c72beb9511
Sjors Spoorendonk <sjors@vicompany.nl>
2022-10-25 11:31:29 +0200

    Vrijwilligersuitje

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [image()],
-  site: 'https://buurthuiskeijenburg.nl',
+	integrations: [
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+		}),
+	],
+	site: "https://buurthuiskeijenburg.nl",
 });
